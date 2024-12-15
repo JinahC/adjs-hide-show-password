cd5b32c660e25f2b9fd82339bf3814e15c6e0ebc
Fridjinah Francois <fridjinah@gravityworksdesign.com>
2024-12-14 20:35:53 -0500

    updated button text on click

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ window.addEventListener('load', () => {
     const passwordContainer = document.querySelector('.input-password');
     const passwordInput = document.getElementById('password');
     const togglePasswordVisibility = document.getElementById('toggle-password-visibility');
+    const togglePasswordText = document.querySelector('.input-password__toggle span');
     
     togglePasswordVisibility.addEventListener('click', (event) => {
         event.preventDefault();
-        console.log(passwordInput.type);
         if (passwordInput.type === 'password') {
             passwordInput.type = 'text';
             passwordContainer.classList.add('show-password');
+            togglePasswordText.textContent = 'Hide password';
         } else {
             passwordInput.type = 'password';
             passwordContainer.classList.remove('show-password');
+            togglePasswordText.textContent = 'Show password';
         }
     });
 });
